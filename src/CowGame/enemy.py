5b451ae5d7d2518e94ffd10fb54694db67f09dd6
David Liang <34115092+davidvliang@users.noreply.github.com>
2023-08-09 15:49:04 -1000

    Update enemy.py

--- src/CowGame/enemy.py
+++ src/CowGame/enemy.py
@@ -1,2 +1,16 @@
-class Enemy():
-    pass
+import pygame
+import params
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.image = pygame.image.load(params.ENEMY_FILEPATH_NORMAL)
+        self.image = pygame.transform.scale(self.image, params.ENEMY_SIZE)
+        self.rect = self.image.get_rect()
+        self.facing_left = True
+
+    def update(self):
+        pass
+
+if __name__ == "__main__":
+    print("ENEMY MONEY")
